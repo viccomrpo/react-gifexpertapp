69e1ceff55e5e332e31ed277eca27430cb357992
Victor Puc <viccom.rpo@gmail.com>
2021-05-01 12:22:54 -0500

    docs add

---
+++ docs/precache-manifest.7362bdfd8e72180af1dcadba69fb9647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ff467ff5273db3e653cb5571fb8089",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56874c471ba0afb03764",
+    "url": "/static/css/main.6b746a24.chunk.css"
+  },
+  {
+    "revision": "e13293c431772057daf3",
+    "url": "/static/js/2.7fae8f10.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.7fae8f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56874c471ba0afb03764",
+    "url": "/static/js/main.d750e403.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
